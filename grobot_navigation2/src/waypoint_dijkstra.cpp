0800c4eefa87442f9d09f5cf9e56c06763b29669
seungmin son <sonsean89@gmail.com>
2024-03-13 21:52:12 +0900

    Fix : DistanceMatrix

--- grobot_navigation2/src/waypoint_dijkstra.cpp
+++ grobot_navigation2/src/waypoint_dijkstra.cpp
@@ -13,6 +13,7 @@
 #include <cmath>
 #include <limits>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -48,10 +49,7 @@ public:
         RCLCPP_INFO(this->get_logger(), "Waypoint: %s", waypoint.c_str());
     }
     initialize_distance_matrix();
-    // double answer = tsp(0, 1); // 시작점 0에서 시작, 비트마스크 1로 시작점 방문 표시
-
-    // RCLCPP_INFO(this->get_logger(), "Minimum travel cost: %f", answer);
-  }
+  } 
 
 private:
 
@@ -63,10 +61,12 @@ private:
     const int N = 11;
     const double inf = numeric_limits<double>::infinity(); //무한대 정의 
     vector<vector<double>> distanceMatrix;  
-    vector<vector<double>> dp;
+    // vector<vector<double>> dp;
+    // double dp;
 
     std::string start;
     std::string end;
+
     // YAML파일 불러오기
     void load_distances_from_yaml() 
     {
@@ -85,11 +85,11 @@ private:
         }
     }
 
-
+    //가중치 matrix 정의 -> distanceMatrix[출발지][도착지] = 가중치 값 
     void initialize_distance_matrix() 
     {
         distanceMatrix.assign(N, vector<double>(N, inf));
-        dp.assign(1 << N, vector<double>(N, inf));
+        // dp.assign(1 << N, vector<double>(N, inf));
 
         // 자기 자신으로 가는 경로의 가중치를 0으로 설정
         for (int i = 0; i < N; ++i) 
@@ -99,7 +99,7 @@ private:
 
         for (auto& p : distance_map) 
         {
-            // yaml파일에서 가져온 문자열에서 'distance_' 부분을 제거
+            // yaml파일에서 가져온 문자열에서 'distance_' 부분을 제거 (인덱싱 과정)
             std::string keyWithoutPrefix = p.first.substr(std::string("distance_").length());
             
             std::string start, end;
@@ -120,50 +120,30 @@ private:
                 }
             }
 
-            std::cerr << "Start: " << start << ", End: " << end << '\n';
-
             int start_idx = std::stoi(start);
             int end_idx = std::stoi(end);
-            distanceMatrix[start_idx][end_idx] = p.second;
+            double distance = p.second;
+
+            // 양방향 경로 설정
+            distanceMatrix[start_idx][end_idx] = distance;
+            distanceMatrix[end_idx][start_idx] = distance;
         }
         
-        // // 행렬 출력용
-        // for (int i = 0; i < distanceMatrix.size(); ++i) {
-        //     for (int j = 0; j < distanceMatrix[i].size(); ++j) {
-        //         if (distanceMatrix[i][j] == inf) {
-        //             cout << "inf ";
-        //         } else {
-        //             cout << distanceMatrix[i][j] << " ";
-        //         }
-        //     }
-        //     cout << "\n";
-        // }
+        // 행렬 출력용
+        for (int i = 0; i < distanceMatrix.size(); ++i) {
+            for (int j = 0; j < distanceMatrix[i].size(); ++j) {
+                if (distanceMatrix[i][j] == inf) {
+                    cout << "inf ";
+                } else {
+                    cout << distanceMatrix[i][j] << " ";
+                }
+            }
+            cout << "\n";
+        }
     }
 
 
-    // Tsp 문제 해결 
-    double tsp(int pos, int bitmask) 
-    {
-        // // 모든 경유지를 방문했다면, 시작점(0)으로 돌아가는 비용 반환
-        // if (bitmask == ((1 << N) - 1)) {
-        //     return distanceMatrix[pos][0];
-        // }
 
-        // // 이미 계산된 경우
-        // if (dp[bitmask][pos] != inf) {
-        //     return dp[bitmask][pos];
-        // }
-
-        // for (int i = 0; i < N; i++) {
-        //     // i번 경유지를 아직 방문하지 않았다면
-        //     if (!(bitmask & (1 << i))) {
-        //         double new_cost = distanceMatrix[pos][i] + tsp(i, bitmask | (1 << i));
-        //         dp[bitmask][pos] = std::min(dp[bitmask][pos], new_cost);
-        //     }
-        // }
-
-        // return dp[bitmask][pos];
-    }
 };
 
 int main(int argc, char **argv)
