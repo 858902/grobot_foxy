fda2f1722bb22fef61ae24cb88edd0387b2a5829
seungmin son <sonsean89@gmail.com>
2024-04-02 19:48:34 +0900

    Change Lidar Merge Param

--- grobot_node/src/lidar_calibration.cpp
+++ grobot_node/src/lidar_calibration.cpp
@@ -285,10 +285,10 @@ private:
     double angle_min_deg_left = -180.0;
     double angle_max_deg_left = 0.0;  
 
-    float robotFrontEnd_ = 0.1;
-    float robotRearEnd_ = 0.1;
-    float robotRightEnd_ = 0.1;
-    float robotLeftEnd_ = 0.1;
+    float robotFrontEnd_ = 0.33;
+    float robotRearEnd_ = 0.29;
+    float robotRightEnd_ = 0.25;
+    float robotLeftEnd_ = 0.25;
 
 };
 
