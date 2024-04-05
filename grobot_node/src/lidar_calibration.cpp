bbddcfa29d2f89297ac471292f94b4060f3570ea
seungmin son <sonsean89@gmail.com>
2024-04-05 21:27:19 +0900

    Fix : Robot Area

--- grobot_node/src/lidar_calibration.cpp
+++ grobot_node/src/lidar_calibration.cpp
@@ -285,8 +285,8 @@ private:
     double angle_min_deg_left = -180.0;
     double angle_max_deg_left = 0.0;  
 
-    float robotFrontEnd_ = 0.37;
-    float robotRearEnd_ = 0.29;
+    float robotFrontEnd_ = 0.29;
+    float robotRearEnd_ = 0.37;
     float robotRightEnd_ = 0.265;
     float robotLeftEnd_ = 0.265;
 
