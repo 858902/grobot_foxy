54354fbaddd905b7719ffc8c964e751b5c618777
seungmin son <sonsean89@gmail.com>
2024-05-02 21:07:28 +0900

    Set External Force Offset

--- grobot_node/src/force_publisher_node.cpp
+++ grobot_node/src/force_publisher_node.cpp
@@ -5,6 +5,8 @@
 #include <deque>
 #include <algorithm>
 #include <cmath>
+#include <string>
+#include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
@@ -23,61 +25,130 @@ public:
         sensor_sub_4 = this->create_subscription<std_msgs::msg::Int32>(
             "sensor_data/sensor4", 10, std::bind(&ForcePublisherNode::sensor_callback_4, this, std::placeholders::_1));
 
+        offset_sub = this->create_subscription<std_msgs::msg::String>(
+            "signal_offset", 10, std::bind(&ForcePublisherNode::signal_offset_callback, this, std::placeholders::_1));
+
         timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&ForcePublisherNode::publish_force, this));
         force_pub = this->create_publisher<std_msgs::msg::Float64MultiArray>("external_force", rclcpp::SystemDefaultsQoS());
     }
 
 private:
-    void sensor_callback_1(const std_msgs::msg::Int32::SharedPtr msg)
+
+    void signal_offset_callback(const std_msgs::msg::String::SharedPtr msg)
     {
-        // force_sensor[0] = msg->data;
-        // update_moving_average(0, msg->data);
-        // update_low_pass_filter(0, msg->data);
-        update_moving_rms(0, msg->data);
+        if (msg->data == "start")
+        {
+            // 오프셋 측정 시작
+            std::fill(std::begin(initial_force_sum_), std::end(initial_force_sum_), 0);
+            std::fill(std::begin(initial_samples_collected_), std::end(initial_samples_collected_), 0);
+            offset_initialized_ = false;
+        }
+        else if (msg->data == "end" && !offset_initialized_)
+        {
+            // 오프셋 측정 완료 및 초기화
+            for (int i = 0; i < 4; i++)
+            {
+                if (initial_samples_collected_[i] > 0)
+                {
+                    offset_[i] = initial_force_sum_[i] / initial_samples_collected_[i];
+                }
+            }
+            offset_initialized_ = true;
+        }
+    }
+
+    void sensor_callback_1(const std_msgs::msg::Int32::SharedPtr msg)
+    {   
+        if (!offset_initialized_)
+        {
+            initial_force_sum_[0] += msg->data;
+            initial_samples_collected_[0]++;
+        }
+
+        else
+        {
+            int adjusted_data = msg->data - offset_[0];
+            update_moving_rms(0, adjusted_data);
+            // force_sensor[0] = msg->data;
+            // update_moving_average(0, msg->data);
+            // update_low_pass_filter(0, msg->data);
+            // update_moving_rms(0, msg->data);
+        }
     }
 
     void sensor_callback_2(const std_msgs::msg::Int32::SharedPtr msg)
-    {
-        // force_sensor[1] = msg->data;
-        // update_moving_average(1, msg->data);
-        // update_low_pass_filter(1, msg->data);
-        update_moving_rms(1, msg->data);
+    {   
+        if (!offset_initialized_)
+        {
+            initial_force_sum_[1] += msg->data;
+            initial_samples_collected_[1]++;
+        }
 
+        else
+        {
+            int adjusted_data = msg->data - offset_[1];
+            update_moving_rms(1, adjusted_data);
+            // force_sensor[1] = msg->data;
+            // update_moving_average(1, msg->data);
+            // update_low_pass_filter(1, msg->data);
+            // update_moving_rms(1, msg->data);
+        }
     }
 
     void sensor_callback_3(const std_msgs::msg::Int32::SharedPtr msg)
     {
-        // force_sensor[2] = msg->data;
-        // update_moving_average(2, msg->data);
-        // update_low_pass_filter(2, msg->data);
-        update_moving_rms(2, msg->data);
+        if (!offset_initialized_)
+        {
+            initial_force_sum_[2] += msg->data;
+            initial_samples_collected_[2]++;
+        }
 
-
+        else
+        {
+            int adjusted_data = msg->data - offset_[2];
+            update_moving_rms(2, adjusted_data);
+            // force_sensor[2] = msg->data;
+            // update_moving_average(2, msg->data);
+            // update_low_pass_filter(2, msg->data);
+            // update_moving_rms(2, msg->data);
+        }
     }
 
     void sensor_callback_4(const std_msgs::msg::Int32::SharedPtr msg)
     {
-        // force_sensor[3] = msg->data;
-        // update_moving_average(3, msg->data);
-        // update_low_pass_filter(3, msg->data);
-        update_moving_rms(3, msg->data);
+        if (!offset_initialized_)
+        {
+            initial_force_sum_[3] += msg->data;
+            initial_samples_collected_[3]++;
+        }
 
-
+        else
+        {
+            int adjusted_data = msg->data - offset_[3];
+            update_moving_rms(3, adjusted_data);
+            // force_sensor[3] = msg->data;
+            // update_moving_average(3, msg->data);
+            // update_low_pass_filter(3, msg->data);
+            // update_moving_rms(3, msg->data);
+        }
     }
 
     void publish_force()
     {
-        double x_force = ((force_sensor[0] + force_sensor[1]) - (force_sensor[2] + force_sensor[3])) * K_x;
-        double yaw_force = (force_sensor[1] - force_sensor[0]) * K_yaw;
+        if (offset_initialized_)
+        {
+            double x_force = ((force_sensor[0] + force_sensor[1]) - (force_sensor[2] + force_sensor[3])) * K_x;
+            double yaw_force = (force_sensor[1] - force_sensor[0]) * K_yaw;
 
-        // 외력 upper limit 설정 
-        x_force = std::clamp(x_force, -0.4, 0.4); 
-        yaw_force = std::clamp(yaw_force, -0.1, 0.1); 
+            // 외력 upper limit 설정 
+            x_force = std::clamp(x_force, -0.4, 0.4); 
+            yaw_force = std::clamp(yaw_force, -0.1, 0.1); 
 
-        std_msgs::msg::Float64MultiArray force_msg;
-        force_msg.data = {x_force, 0, yaw_force};
+            std_msgs::msg::Float64MultiArray force_msg;
+            force_msg.data = {x_force, 0, yaw_force};
 
-        force_pub->publish(force_msg);
+            force_pub->publish(force_msg);
+        }
     }
 
     // Moving Average Fillter 
@@ -93,9 +164,9 @@ private:
     // Low-Pass Filter
     void update_low_pass_filter(int index, int new_data)
     {
-        // Apply the low-pass filter
         force_sensor[index] = alpha * new_data + (1.0 - alpha) * force_sensor[index];
     }
+
     // Moving RMS Filter
     void update_moving_rms(int index, int new_data)
     {
@@ -113,17 +184,22 @@ private:
     static constexpr size_t N = 50; //moving size 
     // std::deque<int> sensor_data[4];
     std::deque<double> sensor_data[4];
+    bool offset_initialized_ = false;
+    double offset_[4] = {0.0, 0.0, 0.0, 0.0};
+    double initial_force_sum_[4] = {0.0, 0.0, 0.0, 0.0};
+    double initial_samples_collected_[4] = {0.0, 0.0, 0.0, 0.0};
+
 
     double force_sensor[4] = {0.0, 0.0, 0.0, 0.0};
     double K_x = 0.005;
     double K_yaw = 0.002;
-
 
     // Subscriber
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sensor_sub_1;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sensor_sub_2;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sensor_sub_3;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sensor_sub_4;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr offset_sub;
 
     //Publisher
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr force_pub;
